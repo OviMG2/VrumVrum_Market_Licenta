f0abd3f589e7dab8bae0024c394aab1b84683067
OviMG2 <ovidiu_marius.guse@stud.etti.upb.ro>
2025-06-13 00:01:54 +0300

    Update permissions.py

--- auto_marketplace/auto_marketplace_backend/listings/permissions.py
+++ auto_marketplace/auto_marketplace_backend/listings/permissions.py
@@ -1,10 +1,7 @@
 from rest_framework import permissions
 
 class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
-    """
-    Permisiune personalizata pentru a permite doar proprietarilor sau
-    administratorilor sa editeze un anunt.
-    """
+    
     
     def has_object_permission(self, request, view, obj):
   
